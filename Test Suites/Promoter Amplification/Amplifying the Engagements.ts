92c849565b69955b064c30d525be182583051a7e
KoushikReddyThelukala <koushikreddy21594@outlook.com>
2019-07-03 18:15:10 +0530

    Just Created Test Case Folders For The Respective Labs.

---
+++ Test Suites/Promoter Amplification/Amplifying the Engagements.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amplifying the Engagements</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>842b6790-8830-4dee-9340-f7dee402a8c2</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/Promoter Amplification/Amplifying the Posts.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amplifying the Posts</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f770d42b-4d7a-464d-887e-d5bde9bfc2d2</testSuiteGuid>
+</TestSuiteEntity>
